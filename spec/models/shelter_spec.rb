50ddc0a150062987222d700bbb89c0917b5d6b9d
Phillip Kamps <phillip.kamps@gmail.com>
2022-04-06 17:52:52 -0600

    Add test to pre-work to hit coverage

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -1,11 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Shelter, type: :model do
-  describe 'relationships' do
+  describe "relationships" do
     it { should have_many(:pets) }
   end
 
-  describe 'validations' do
+  describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:city) }
     it { should validate_presence_of(:rank) }
@@ -13,69 +13,75 @@ RSpec.describe Shelter, type: :model do
   end
 
   before(:each) do
-    @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
-    @shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+    @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+    @shelter_2 = Shelter.create(name: "RGV animal shelter", city: "Harlingen, TX", foster_program: false, rank: 5)
+    @shelter_3 = Shelter.create(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: true, rank: 10)
 
-    @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
-    @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-    @pet_3 = @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
-    @pet_4 = @shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 5, adoptable: true)
+    @pet_1 = @shelter_1.pets.create(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: false)
+    @pet_2 = @shelter_1.pets.create(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
+    @pet_3 = @shelter_3.pets.create(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true)
+    @pet_4 = @shelter_1.pets.create(name: "Ann", breed: "ragdoll", age: 5, adoptable: true)
   end
 
-  describe 'class methods' do
-    describe '#search' do
-      it 'returns partial matches' do
-        expect(Shelter.search('Fancy')).to eq([@shelter_3])
+  describe "class methods" do
+    describe "#search" do
+      it "returns partial matches" do
+        expect(Shelter.search("Fancy")).to eq([@shelter_3])
       end
     end
-    
-    describe '#shelters_w_applications' do
+
+    describe "#shelters_w_applications" do
       it "returns shelters that have a pending application" do
-        @smith_app = Application.create!(name: "Bobby Smith", address: "3245 E 1st", city: "Lakewood", state: "CO", zipcode: "80026", description: "Im Awesome!", app_status: "Pending") 
-        @jones_app = Application.create!(name: "Kim Jones", address: "3245 E 1st", city: "Lakewood", state: "CO", zipcode: "80026", description: "Im Awesome!", app_status: "Pending") 
-        @adams_app = Application.create!(name: "Steve Adams", address: "3245 E 1st", city: "Lakewood", state: "CO", zipcode: "80026", description: "beats dogs!", app_status: "Rejected") 
+        @smith_app = Application.create!(name: "Bobby Smith", address: "3245 E 1st", city: "Lakewood", state: "CO", zipcode: "80026", description: "Im Awesome!", app_status: "Pending")
+        @jones_app = Application.create!(name: "Kim Jones", address: "3245 E 1st", city: "Lakewood", state: "CO", zipcode: "80026", description: "Im Awesome!", app_status: "Pending")
+        @adams_app = Application.create!(name: "Steve Adams", address: "3245 E 1st", city: "Lakewood", state: "CO", zipcode: "80026", description: "beats dogs!", app_status: "Rejected")
         PetApplication.create(pet: @pet_1, application: @smith_app)
         PetApplication.create(pet: @pet_3, application: @adams_app)
         expect(Shelter.shelters_w_applications).to eq([@shelter_1])
         expect(Shelter.shelters_w_applications).to_not eq([@shelter_3])
-      end 
-    end 
+      end
+    end
 
-    describe '#order_by_recently_created' do
-      it 'returns shelters with the most recently created first' do
+    describe "#order_by_recently_created" do
+      it "returns shelters with the most recently created first" do
         expect(Shelter.order_by_recently_created).to eq([@shelter_3, @shelter_2, @shelter_1])
       end
     end
 
-    describe '#order_by_number_of_pets' do
-      it 'orders the shelters by number of pets they have, descending' do
+    describe "#order_by_number_of_pets" do
+      it "orders the shelters by number of pets they have, descending" do
         expect(Shelter.order_by_number_of_pets).to eq([@shelter_1, @shelter_3, @shelter_2])
+      end
+    end
+
+    describe "#reverse_alphabetical_order" do
+      it "orders shelters reverse alphabetiacally" do
+        expect(Shelter.reverse_alphabetical_order).to eq([@shelter_2, @shelter_3, @shelter_1])
       end
     end
   end
 
-  describe 'instance methods' do
-    describe '.adoptable_pets' do
-      it 'only returns pets that are adoptable' do
+  describe "instance methods" do
+    describe ".adoptable_pets" do
+      it "only returns pets that are adoptable" do
         expect(@shelter_1.adoptable_pets).to eq([@pet_2, @pet_4])
       end
     end
 
-    describe '.alphabetical_pets' do
-      it 'returns pets associated with the given shelter in alphabetical name order' do
+    describe ".alphabetical_pets" do
+      it "returns pets associated with the given shelter in alphabetical name order" do
         expect(@shelter_1.alphabetical_pets).to eq([@pet_4, @pet_2])
       end
     end
 
-    describe '.shelter_pets_filtered_by_age' do
-      it 'filters the shelter pets based on given params' do
+    describe ".shelter_pets_filtered_by_age" do
+      it "filters the shelter pets based on given params" do
         expect(@shelter_1.shelter_pets_filtered_by_age(5)).to eq([@pet_4])
       end
     end
 
-    describe '.pet_count' do
-      it 'returns the number of pets at the given shelter' do
+    describe ".pet_count" do
+      it "returns the number of pets at the given shelter" do
         expect(@shelter_1.pet_count).to eq(3)
       end
     end
