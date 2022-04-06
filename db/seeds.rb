bb00453fcf3f9b13320a367aa16dbdeccd86723d
Phillip Kamps <phillip.kamps@gmail.com>
2022-04-06 17:32:54 -0600

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,12 @@ generic = fancy_shelter.pets.create(name: "Generic", age: 4, breed: "Pit", adopt
 bobby = Application.create(name: "Bobby Smith", address: "3245 E 1st", city: "Lakewood", state: "CO", zipcode: "80026", app_status: "In Progress")
 joe = Application.create(name: "Joe", address: "4455 1st", city: "Denver", state: "CO", zipcode: "85555", app_status: "In Progress")
 susan = Application.create(name: "Susan", address: "4678 N st", city: "Lakewood", state: "CO", zipcode: "82345", app_status: "In Progress")
+
+app1 = PetApplication.create(pet: scooby, application: bobby)
+app2 = PetApplication.create(pet: ralph, application: bobby)
+app3 = PetApplication.create(pet: scooby, application: joe)
+app4 = PetApplication.create(pet: bilbo, application: joe)
+app5 = PetApplication.create(pet: bilbo, application: susan)
+app6 = PetApplication.create(pet: frank, application: susan)
+app7 = PetApplication.create(pet: generic, application: bobby)
+app8 = PetApplication.create(pet: generic, application: susan)
